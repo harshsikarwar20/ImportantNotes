8edda93a59be339ec61869892b996c2d4701dd94
Harsh Sikarwar <123385605+harshsikarwar20@users.noreply.github.com>
2023-11-16 11:21:38 +0530

    Update LooseCoupling.java

--- LooseCoupling.java
+++ LooseCoupling.java
@@ -1,21 +1,21 @@
 // Java program to illustrate  
 // loose coupling concept 
-public interface Topic { 
+interface Topic { 
     void understand(); 
 } 
 class Topic1 implements Topic { 
     public void understand(){ 
-            System.out.println("Got it"); 
+        System.out.println("Got it"); 
     } 
 } 
 class Topic2 implements Topic { 
     public void understand(){ 
-            System.out.println("understand"); 
+        System.out.println("understand"); 
     } 
 } 
-public class Subject { 
+public class Volume { 
     public static void main(String[] args){ 
-            Topic t = new Topic1(); 
-            t.understand(); 
+        Topic t = new Topic1(); 
+        t.understand(); 
     } 
-} 
+}
